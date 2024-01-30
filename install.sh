9aee92a0d5daf0c6e8a74781d3372fb020a83150
hhroot <105219296+735840086@users.noreply.github.com>
2024-01-30 23:02:12 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 ﻿#!/bin/bash
 
-VERSION="kkminer"
-DOWNLOAD_HOST="https://github.com/735840086/kkminer/main/kkminer"
+VERSION="3.9.4"
+DOWNLOAD_HOST="https://github.com/735840086/kkminer/main/kkminer-3.9.4"
 ORIGIN_EXEC="kkminer-${VERSION}"
 
 SERVICE_NAME="kkminerservice"
